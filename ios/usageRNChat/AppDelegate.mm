4da052b3074b30afd118fb7bf9fb382d02c715c4
David Aguirre <juandavid.aguirre@qubika.com>
2024-01-29 22:36:56 -0500

    Initial qubika template commit

--- ios/usageRNChat/AppDelegate.mm
+++ ios/usageRNChat/AppDelegate.mm
@@ -7,7 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"QubikaApp";
+  self.moduleName = @"usage_RNChat";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
